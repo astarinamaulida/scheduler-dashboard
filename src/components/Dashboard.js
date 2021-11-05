16ba3b3ed081b1c056f748be811fcdd9b3d09337
Astarina Maulida <astarina.maulida@gmail.com>
2021-11-05 06:00:39 -0400

    Remove id prop

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -59,9 +59,9 @@ class Dashboard extends Component {
       .map(panel => (
         <Panel
           key={panel.id}
-          id={panel.id}
           label={panel.label}
           value={panel.value}
+          onSelect={event => this.selectPanel(panel.id)}
         />
       ));
 
